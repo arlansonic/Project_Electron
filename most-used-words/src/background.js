be1db286f2e0bbaed6ba7e261723a8873fde6d7e
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-04-19 11:50:06 -0400

    Iniciando com Electron

--- most-used-words/src/background.js
+++ most-used-words/src/background.js
@@ -16,25 +16,31 @@ async function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      
+
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
     }
+  })  
+  // Colocando Title e Versão no APP
+  win.webContents.on('did-finish-load', () => {
+    const { title, version} = require('../package.json')
+    win.setTitle(`${title} :: ${version}`)
+    console.log('Terminou de carregar')
   })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    // if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
-}
 
+}
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
