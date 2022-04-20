3739c719dc58f37a61a90b572e304fa31eeccb19
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-04-20 15:08:16 -0400

    Criando interface

--- most-used-words/src/background.js
+++ most-used-words/src/background.js
@@ -2,7 +2,7 @@
 
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -16,31 +16,32 @@ async function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-
+      
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
     }
-  })  
-  // Colocando Title e Versão no APP
-  win.webContents.on('did-finish-load', () => {
-    const { title, version} = require('../package.json')
-    win.setTitle(`${title} :: ${version}`)
+  })
+
+   // Colocando Title e Versão no APP
+   win.webContents.on('did-finish-load', () => {
+    const { title, version, author} = require('../package.json')
+    win.setTitle(`${title} :: ${version}, ${author}`)
     console.log('Terminou de carregar')
   })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    // if (!process.env.IS_TEST) win.webContents.openDevTools()
+    if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
-
 }
+
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
@@ -63,7 +64,7 @@ app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS3_DEVTOOLS)
+      await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
       console.error('Vue Devtools failed to install:', e.toString())
     }
---
+++ most-used-words/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+<v-container-fluid>
+    <div class='pills'>
+        <Pill v-for="word in groupWords" :key="word.name" :name="word.name" :amount="word.amount"/>
+    </div>
+</v-container-fluid>
+  
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+    components: {
+        Pill,
+    },
+    data: function () {
+        return{
+            groupWords:[
+                {name: 'i', amount:1200},
+                {name: 'you', amount:900},
+                {name: 'he', amount:800},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
