62d5a34fc95be8de507a778a4c6a42c9d5a32dbc
Arlan Marreiro <arlan_marreiro@cal-comp.com.br>
2022-05-17 16:12:07 -0400

    Deleting Projeto Old

--- most-used-words/src/backend/index.js
+++ most-used-words/src/backend/index.js
@@ -1,19 +1,13 @@
 const { ipcMain } = require('electron')
+const groupWords = require('./groupWords')
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
 
 ipcMain.on('process-subtitle', (event, paths) => {
     pathsToRows(paths)
         .then(rows => prepareData(rows))
-        .then(words => console.log(words))
-        .then(() => {
-            event.reply('process-subtitle', [
-                { name: "i", amount: 1200 },
-                { name: "you", amount: 900 },
-                { name: "he", amount: 800 },
-                { name: "she", amount: 1200 },
-                { name: "our", amount: 900 },
-                { name: "house", amount: 800 },
-            ])
+        .then(words => groupWords(words))
+        .then(groupedWords => {
+            event.reply('process-subtitle', groupedWords)
         })
 })
